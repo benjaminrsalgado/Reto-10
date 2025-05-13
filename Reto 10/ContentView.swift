c923aed2a9c165bfe4c17b5b7eaaf015148e5882
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-13 12:47:46 -0600

    i did some changes

--- Reto 10/ContentView.swift
+++ Reto 10/ContentView.swift
@@ -7,18 +7,51 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct WatherMainView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Mexico City")
+                .font(.title)
+                .foregroundColor(.black)
+                .padding()
+            Text("24°C")
+                .font(.title)
+                .foregroundColor(.black)
+                .padding()
+            Image(systemName:"sun.max.fill")
+                .font(.system(size: 40, weight: .bold))
+                .foregroundColor(.yellow)
         }
+        WeatherDetailView()
         .padding()
     }
 }
 
+struct WeatherDetailView: View{
+    var body: some View{
+        Text("7 Days Forecast")
+            .font(.title)
+            .foregroundColor(.black)
+            .padding()
+        HStack{
+         
+        }
+    }
+}
+
+struct LunesView: View{
+    var body: some View{
+        VStack{
+            Text("Lunes")
+            Image(systemName: "star.fill")
+                .font(.system(size: 40, weight: .bold))
+                .foregroundColor(.yellow)
+        }
+    }
+}
+
+
+
 #Preview {
-    ContentView()
+    WatherMainView()
 }
